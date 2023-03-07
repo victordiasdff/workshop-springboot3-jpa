51674a224c2251e319fb536fb388d34e909f0944
Victor Dias <victordiasdff@gmail.com>
2023-03-07 11:16:33 -0300

    Tratamento de Exceção Delete

--- course/src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ course/src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import services.exceptions.DatabaseException;
 import services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -20,5 +21,11 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> DatabaseException(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+}
 }
